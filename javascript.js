73c9155144246295485c9d83728ce7387498605f
Ana Flávia Vilela <anaflaviavilela2@gmail.com>
2024-05-25 16:54:41 -0300

    Inicio de projeto.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const telegram = document.querySelector('.fa-telegram');
+    
+    telegram.addEventListener('click', () => {
+        window.open('https://www.instagram.com/grangeirobet/', '_blank');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const instagram = document.querySelector('.fa-instagram');
+        
+    instagram.addEventListener('click', () => {
+        window.open('https://www.instagram.com/grangeirobet/', '_blank');
+    });
+});
